d69f34bd9ff9c522c19ea373efeccba8b2171849
Akalanka Ekanayake <33100218+cutenzinc@users.noreply.github.com>
2020-09-15 09:50:34 +0530

    wokring release 2018

--- js/User/user_validation.js
+++ js/User/user_validation.js
@@ -2,6 +2,10 @@
 //Current Action = save/1 , update/2
 function userRequiredFieldHandler(frm_data, action, required_class) {
     var response = true;
+    if (frm_data.initials.length == 0) {
+        toastr.error('Initials Required!');
+        response = false;
+    }
     if (frm_data.date_of_birth.length == 0) {
         toastr.error('Date Of Birth Required!');
         response = false;
